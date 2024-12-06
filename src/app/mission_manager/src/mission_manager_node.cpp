d2d4ee19cc26b5d4b3a8e7ccbf9ba8503d9fe27e
BagasHebat <bagastegar03@gmail.com>
2024-12-06 19:56:47 +0700

    Implementing Word Spacing, Comment, and Indentation
    Adding Comments, Add Spaces between line, And improving the Indentation for readable code.

--- src/app/mission_manager/src/mission_manager_node.cpp
+++ src/app/mission_manager/src/mission_manager_node.cpp
@@ -23,25 +23,32 @@ ScenarioRunner::ScenarioRunner(const std::string& node_name, const rclcpp::NodeO
     this->declare_parameter("mission_manager/ns", "");
     this->declare_parameter("mission_manager/loop_rate_hz", 100.0);
     this->declare_parameter("mission_manager/id_merging_lane", 0);
+        
     this->declare_parameter("mission_manager/num_of_lane", 3);
     this->declare_parameter("mission_manager/num_of_merging_vehicles", 3);
     this->declare_parameter("mission_manager/detection_range", 15.0);
-    this->declare_parameter("mission_manager/object_max_accel", 3.0);
+        
+    this->declare_parameter("mission_manager/object_max_accel", 3.0);   
     this->declare_parameter("mission_manager/merging_section_length", 25.0);
     this->declare_parameter("mission_manager/merging_interval", 10.0);
+        
     this->declare_parameter("mission_manager/merging_speed", 30.0);
     this->declare_parameter("mission_manager/merging_target_speed", 50.0);
+        
     ProcessParams();
 
     RCLCPP_INFO(this->get_logger(), "vehicle_namespace: %s", cfg_.vehicle_namespace.c_str());
     RCLCPP_INFO(this->get_logger(), "loop_rate_hz: %f", cfg_.loop_rate_hz);
     RCLCPP_INFO(this->get_logger(), "id_merging_lane: %d", cfg_.id_merging_lane);
+        
     RCLCPP_INFO(this->get_logger(), "num_of_lane: %d", cfg_.num_of_lane);
     RCLCPP_INFO(this->get_logger(), "num_of_merging_vehicles: %d", cfg_.num_of_merging_vehicles);
     RCLCPP_INFO(this->get_logger(), "detection_range: %f", cfg_.detection_range);
+        
     RCLCPP_INFO(this->get_logger(), "object_max_accel: %f", cfg_.object_max_accel);
     RCLCPP_INFO(this->get_logger(), "merging_section_length: %f", cfg_.merging_section_length);
     RCLCPP_INFO(this->get_logger(), "merging_interval: %f", cfg_.merging_interval);
+        
     RCLCPP_INFO(this->get_logger(), "merging_speed: %f", cfg_.merging_speed);
     RCLCPP_INFO(this->get_logger(), "merging_target_speed: %f", cfg_.merging_target_speed);
     
@@ -52,13 +59,18 @@ ScenarioRunner::ScenarioRunner(const std::string& node_name, const rclcpp::NodeO
     
     // Subscriber init
     s_vehicle_state_ = this->create_subscription<ad_msgs::msg::VehicleState> (
-        "vehicle_state", qos_profile, std::bind(&ScenarioRunner::CallbackVehicleState, this, std::placeholders::_1));
+        "vehicle_state", 
+        qos_profile, 
+        std::bind(&ScenarioRunner::CallbackVehicleState, this, std::placeholders::_1)
+        );
     
     // Publisher init
     p_detected_mission_ = this->create_publisher<ad_msgs::msg::Mission>(
-        "mission", qos_profile);
+        "mission", 
+        qos_profile);
     p_display_mission_ = this->create_publisher<ad_msgs::msg::MissionDisplay>(
-        "mission_display", qos_profile);
+        "mission_display", 
+        qos_profile);
 
     // Timer init
     t_run_node_ = this->create_wall_timer(
@@ -73,14 +85,17 @@ ScenarioRunner::ScenarioRunner(const std::string& node_name, const rclcpp::NodeO
     alg_mission_road_condition_ = std::make_unique<MissionRoadCondition>(i_ref_lanes_, i_id_center_, cfg_);
     alg_mission_road_slope_ = std::make_unique<MissionRoadSlope>(i_ref_lanes_, i_id_center_, cfg_);
     RCLCPP_INFO(this->get_logger(), "Road Slope Mission Generation Complete");
+        
     alg_mission_scc_ = std::make_unique<MissionSCC>(i_ref_lanes_, i_id_center_, cfg_); 
     RCLCPP_INFO(this->get_logger(), "SCC Mission Generation Complete");
+        
     alg_mission_speed_limit_ = std::make_unique<MissionSpeedLimit>(i_ref_lanes_, i_id_center_, cfg_);
     RCLCPP_INFO(this->get_logger(), "Speed Limit Mission Generation Complete");
+        
     alg_mission_merge_ = std::make_unique<MissionMerge>(i_ref_lanes_, i_id_center_, cfg_);
     RCLCPP_INFO(this->get_logger(), "Merge Mission Generation Complete");
-    time_prev_ = this->now().seconds();
-
+        
+    time_prev_ = this->now().seconds(); // Get current time using second as the unit
     RCLCPP_WARN_STREAM(this->get_logger(), "Initialize node (Period: " << cfg_.loop_rate_hz << " Hz)");
 }
 
@@ -90,25 +105,37 @@ void ScenarioRunner::ProcessParams() {
     this->get_parameter("mission_manager/ns", cfg_.vehicle_namespace);
     this->get_parameter("mission_manager/loop_rate_hz", cfg_.loop_rate_hz);
     this->get_parameter("mission_manager/id_merging_lane", cfg_.id_merging_lane);
+    
     this->get_parameter("mission_manager/num_of_lane", cfg_.num_of_lane);
     this->get_parameter("mission_manager/num_of_merging_vehicles", cfg_.num_of_merging_vehicles);
     this->get_parameter("mission_manager/detection_range", cfg_.detection_range);
+    
     this->get_parameter("mission_manager/object_max_accel", cfg_.object_max_accel);
     this->get_parameter("mission_manager/merging_section_length", cfg_.merging_section_length);
     this->get_parameter("mission_manager/merging_interval", cfg_.merging_interval);
+    
     this->get_parameter("mission_manager/merging_speed", cfg_.merging_speed);
     this->get_parameter("mission_manager/merging_target_speed", cfg_.merging_target_speed);
 }
 
 void ScenarioRunner::Run() {
-    auto current_time = this->now();
-    RCLCPP_INFO_THROTTLE(this->get_logger(), *get_clock(), 1000, "Running ...");
+    auto current_time = this->now(); // Get current_time of the system and assign to the variable
+    
+    RCLCPP_INFO_THROTTLE(
+        this->get_logger(), 
+        *get_clock(), 
+        1000, 
+        "Running ...");
 
     // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
     // Get subscribe variables
     // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
     if (b_is_vehicle_state_ == false) {
-        RCLCPP_ERROR_THROTTLE(this->get_logger(), *this->get_clock(), 1000, "Wait for Vehicle State...");
+        RCLCPP_ERROR_THROTTLE(
+            this->get_logger(), 
+            *this->get_clock(), 
+            1000, 
+            "Wait for Vehicle State...");
         return;
     }
 
@@ -121,12 +148,13 @@ void ScenarioRunner::Run() {
     // Algorithm
     // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
     double time_dt = current_time.seconds() - time_prev_;
-    time_prev_ = current_time.seconds();
+    time_prev_ = current_time.seconds(); // Get current_time of the system and assign to the variable using second as the unit
 
     //  Get detected mission
     std::string road_condition                          = alg_mission_road_condition_->RunAlgorithm(vehicle_state, cfg_);
     std::string road_slope                              = alg_mission_road_slope_->RunAlgorithm(vehicle_state, cfg_);
     double speed_limit                                  = alg_mission_speed_limit_->RunAlgorithm(vehicle_state, cfg_);
+    
     std::vector<interface::MissionObject> scc_objects   = alg_mission_scc_->RunAlgorithm(vehicle_state, i_ref_lanes_, time_dt, cfg_);
     std::vector<interface::MissionObject> merge_objects = alg_mission_merge_->RunAlgorithm(vehicle_state, i_ref_lanes_, time_dt, cfg_);
 
@@ -134,14 +162,25 @@ void ScenarioRunner::Run() {
     std::vector<interface::MissionRegion> display_road_condition = alg_mission_road_condition_->GetRoadCondition();
     std::vector<interface::MissionRegion> display_road_slope     = alg_mission_road_slope_->GetRoadSlope();
     std::vector<interface::MissionRegion> display_speed_limit    = alg_mission_speed_limit_->GetSpeedLimit();
+    
     std::vector<interface::MissionObject> display_scc_objects    = alg_mission_scc_->GetSCCObjects();
     std::vector<interface::MissionObject> display_merge_objects  = alg_mission_merge_->GetMergeObjects();
 
     // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
     // Publish output
     // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
-    p_detected_mission_->publish(ros2_bridge::UpdateMission(scc_objects, merge_objects, road_condition, road_slope, speed_limit));
-    p_display_mission_->publish(ros2_bridge::UpdateMissionDisplay(display_scc_objects, display_merge_objects, display_road_slope, display_road_condition, display_speed_limit));
+    p_detected_mission_->publish(ros2_bridge::UpdateMission(scc_objects, 
+        merge_objects, 
+        road_condition, 
+        road_slope, 
+        speed_limit));
+    
+    p_display_mission_->publish(ros2_bridge::UpdateMissionDisplay(
+        display_scc_objects, 
+        display_merge_objects, 
+        display_road_slope, 
+        display_road_condition, 
+        display_speed_limit));
 }
 
 int main(int argc, char **argv) {
@@ -149,7 +188,11 @@ int main(int argc, char **argv) {
 
     // Initialize node
     rclcpp::init(argc, argv);
+
+    //  Run The Loop
     rclcpp::spin(std::make_shared<ScenarioRunner>(node_name));
+
+    // Stop the Node
     rclcpp::shutdown();
 
     return 0;
