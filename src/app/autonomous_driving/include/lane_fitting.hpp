8b2fc80ef7602ff68033ad90b6926e66b196740a
한석희 <149641221+HanSeokHui@users.noreply.github.com>
2024-12-17 13:17:30 +0900

    Update lane_fitting.hpp

--- src/app/autonomous_driving/include/lane_fitting.hpp
+++ src/app/autonomous_driving/include/lane_fitting.hpp
@@ -1,25 +1,39 @@
-#ifndef LANE_FITTING_HPP
-#define LANE_FITTING_HPP
+#ifndef LANE_PROCESSING_NODE_HPP
+#define LANE_PROCESSING_NODE_HPP
 
 #include <vector>
 #include <geometry_msgs/msg/point.hpp>
 #include <eigen3/Eigen/Dense>
+#include <rclcpp/rclcpp.hpp>
+#include "ad_msgs/msg/lane_point_data.hpp"
+#include "ad_msgs/msg/polyfit_lane_data.hpp"
+#include "ad_msgs/msg/polyfit_lane_data_array.hpp"
 
-class LaneFitting {
+class LaneProcessingNode : public rclcpp::Node {
 public:
-    LaneFitting(int window_size, int poly_order);
+    explicit LaneProcessingNode(const std::string& node_name, const rclcpp::NodeOptions& options = rclcpp::NodeOptions());
+    ~LaneProcessingNode() = default;
 
-    std::vector<double> generateSavitzkyGolayKernel();
-    std::vector<double> applySavitzkyGolayFilter(const std::vector<double>& data, const std::vector<double>& kernel);
+private:
+    void CallbackLanePoints(const ad_msgs::msg::LanePointData::SharedPtr msg);
 
+    // Lane Detection and Clustering
     std::vector<std::vector<geometry_msgs::msg::Point>> dbscanClustering(
         const std::vector<geometry_msgs::msg::Point>& points, double eps, int min_points, double x_weight);
 
+    // Lane Fitting and Filtering
+    std::vector<double> generateSavitzkyGolayKernel(int window_size, int poly_order);
+    std::vector<double> applySavitzkyGolayFilter(const std::vector<double>& data, const std::vector<double>& kernel);
     Eigen::VectorXd fitPolynomial(const std::vector<geometry_msgs::msg::Point>& points, int order);
 
-private:
-    int window_size_;
-    int poly_order_;
+    // Subscribers and Publishers
+    rclcpp::Subscription<ad_msgs::msg::LanePointData>::SharedPtr s_lane_points_;
+    rclcpp::Publisher<ad_msgs::msg::PolyfitLaneDataArray>::SharedPtr p_poly_lanes_;
+
+    // Parameters
+    double eps_, x_weight_;
+    int min_points_, window_size_, poly_order_;
+    double lane_width_;
 };
 
-#endif // LANE_FITTING_HPP
+#endif // LANE_PROCESSING_NODE_HPP
